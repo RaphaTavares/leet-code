c6d15bfc54b7c44b828fb4f2e5efa394d5cab3e4
Raphael Tavares <raphael_tavares2000@hotmail.com>
2024-06-07 00:23:33 -0300

    Time: 45 ms (99.19%) | Memory: 41.9 MB (79.39%) - LeetSync

--- 242-valid-anagram/valid-anagram.cs
+++ 242-valid-anagram/valid-anagram.cs
@@ -1,22 +1,33 @@
 public class Solution {
     public bool IsAnagram(string s, string t) {
 
-        var isSameSize = s.Length == t.Length;
-
-        if(!isSameSize)
+        if(s.Length != t.Length)
             return false;
+
+        var countS = new Dictionary<char, int>();
+        var countT = new Dictionary<char, int>();
 
         for(var i = 0; i < s.Length; i++)
         {
-            if(t.Contains(s[i])){
-                var index = t.IndexOf(s[i]);
+            if(countS.ContainsKey(s[i])){
+                countS[s[i]] += 1;
+            }
+            else{
+                countS[s[i]] = 1;
+            }
 
-                t = t.Remove(index, 1);
+            if(countT.ContainsKey(t[i])){
+                countT[t[i]] += 1;
+            }
+            else{
+                countT[t[i]] = 1;
             }
         }
         
-        if(t.Length != 0)
-            return false;
+        foreach(var key in countS.Keys){
+            if(!countT.ContainsKey(key) || countT[key] != countS[key])
+                return false;
+        }
         
         return true;
     }
