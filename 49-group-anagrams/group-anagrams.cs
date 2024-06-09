479139564846e74169a3ec9a4b57c7ace62650cd
Raphael Tavares <raphael_tavares2000@hotmail.com>
2024-06-09 15:05:13 -0300

    Time: 158 ms (45.10%) | Memory: 87.3 MB (18.25%) - LeetSync

--- 49-group-anagrams/group-anagrams.cs
+++ 49-group-anagrams/group-anagrams.cs
@@ -1,39 +1,44 @@
 public class Solution {
     public IList<IList<string>> GroupAnagrams(string[] strs) {
         
-        IList<IList<string>> result = new List<IList<string>>();
-        
-        for (int i = 0; i < strs.Length; i++) {
-            bool added = false;
-            
-            foreach (var group in result) {
-                if (IsAnagram(group[0], strs[i])) {
-                    group.Add(strs[i]);
-                    added = true;
-                    break;
-                }
-            }
-            
-            if (!added) {
-                result.Add(new List<string> { strs[i] });
-            }
-        }
-        
-        return result;
-    }
-    
+        var anagramGroups = new Dictionary<string, List<string>>();
 
-    private bool IsAnagram(string s1, string s2) {
-        if (s1.Length != s2.Length) return false;
-        
-        var count = new int[26];
-        foreach (var c in s1) count[c - 'a']++;
-        foreach (var c in s2) {
-            if (--count[c - 'a'] < 0) return false;
+        var usedIndexes = new List<int>();
+
+        for(var i = 0; i < strs.Length; i++)
+        {
+            var orderedStr = new string(strs[i].OrderBy(x => x).ToArray());
+
+            if(!anagramGroups.ContainsKey(orderedStr))
+                anagramGroups.Add(orderedStr, new List<string>(){strs[i]});
+
+            else
+                anagramGroups[orderedStr].Add(strs[i]);
         }
-        
-        return true;
+
+        return anagramGroups.Values.ToList<IList<string>>();
     }
 }
 
+public static class ExtensionMethods {
+        public static bool IsAnagram(this string firstString, string secondString)
+{
+    if(firstString.Length != secondString.Length)
+        return false;
 
+    int[] charCount = new int[26];
+
+    for(var i = 0; i < firstString.Length; i++){
+        charCount[firstString[i] - 'a']++;
+        charCount[secondString[i] - 'a']--;
+    }
+
+    for(var i = 0; i < charCount.Length; i++)
+    {
+        if(charCount[i] != 0)
+            return false;
+    }
+
+    return true;
+}
+}
