170b7ff6843e78c7a47483a4fddaca10496c0876
Raphael Tavares <raphael_tavares2000@hotmail.com>
2024-06-10 23:02:22 -0300

    Time: 70 ms (88.26%) | Memory: 9.7 MB (50.22%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.go
@@ -0,0 +1,14 @@
+func containsDuplicate(nums []int) bool {
+    
+    hashSet := make(map[int]bool);
+
+    for _, num := range nums{
+        if hashSet[num]{
+            return true
+        }
+
+        hashSet[num] = true
+    }
+
+    return false
+}
