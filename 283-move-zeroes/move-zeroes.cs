2dae3a967fca340fe868b4b2ce685e0e9bdf447b
Raphael Tavares <raphael_tavares2000@hotmail.com>
2024-06-17 18:31:03 -0300

    Time: 532 ms (5.07%) | Memory: 57.3 MB (76.72%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public void MoveZeroes(int[] nums) {
+        
+        for(int j = 0; j < nums.Length; j++){
+            for(int i = nums.Length - 1; i >= 0; i--){
+                if(nums[i] == 0 && i < nums.Length - 1){
+                    var temp = nums[i + 1];
+                    nums[i + 1] = 0;
+                    nums[i] = temp;
+            }
+        }
+        }
+        
+    }
+}
