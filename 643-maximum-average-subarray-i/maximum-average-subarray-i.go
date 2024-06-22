4e32866f2c4b52787b7b9192d3a36a945220a714
Raphael Tavares <raphael_tavares2000@hotmail.com>
2024-06-22 09:02:23 -0300

    Time: 141 ms (45.33%) | Memory: 15.5 MB (5.03%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.go
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.go
@@ -1,20 +1,19 @@
-
-import "math"
 func findMaxAverage(nums []int, k int) float64 {
-    
-    biggestSumOfKElements := math.MinInt64;
+    sum := 0
 
-    for i := 0; i <= len(nums) - k; i++ {
-        sumOfKElements := 0
+    for i := 0; i < k; i++ {
+        sum += nums[i]
+    }
 
-        for j := 0; j < k; j++ {
-            sumOfKElements += nums[i+j]
-        }
+    maxSum := sum
 
-        if sumOfKElements > biggestSumOfKElements{
-            biggestSumOfKElements = sumOfKElements
+    for i := k; i < len(nums); i++ {
+        sum += nums[i] - nums[i - k]
+
+        if sum > maxSum{
+            maxSum = sum
         }
     }
 
-    return float64(biggestSumOfKElements) / float64(k)
-} 
+    return float64(maxSum) / float64(k)
+}
