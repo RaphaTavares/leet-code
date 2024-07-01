484f12a019a6be2b2f9f552c7073471c4dd553c0
Raphael Tavares <raphael_tavares2000@hotmail.com>
2024-07-01 20:21:29 -0300

    Time: 575 ms (6.45%) | Memory: 122.7 MB (45.97%) - LeetSync

---
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.cs
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode DeleteMiddle(ListNode head) {
+        
+        if(head.next == null)
+            return null;
+
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode prev = null;
+
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            prev = slow;
+            slow = slow.next;
+        }
+
+        if(prev != null)
+            prev.next = slow.next;
+
+        return head;
+    }
+}
