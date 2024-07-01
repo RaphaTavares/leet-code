6815d4bdfb8b85432c9c4f7e952a64d47a04d098
Raphael Tavares <raphael_tavares2000@hotmail.com>
2024-06-30 23:26:12 -0300

    Time: 11 ms (77.33%) | Memory: 6.2 MB (83.97%) - LeetSync

---
+++ 605-can-place-flowers/can-place-flowers.go
@@ -0,0 +1,22 @@
+func canPlaceFlowers(flowerbed []int, n int) bool {
+    count := 0
+    length := len(flowerbed)
+    for i := 0; i < length; i++ {
+
+        if flowerbed[i] == 0 {
+            prevEmpty := (i == 0) || (flowerbed[i-1] == 0)
+            nextEmpty := (i == length-1) || (flowerbed[i+1] == 0)
+
+            if prevEmpty && nextEmpty {
+                flowerbed[i] = 1
+                count++
+                if count >= n {
+                return true
+                }
+                i++
+            }
+        }
+    }
+
+    return count >= n
+}
