d28ec66d6c42cef5ba59f954925cf104a3273a9d
Raphael Tavares <raphael_tavares2000@hotmail.com>
2024-07-04 20:48:23 -0300

    Time: 77 ms (13.56%) | Memory: 42.8 MB (10.21%) - LeetSync

---
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cs
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int MaxDepth(TreeNode root) {
+        if(root == null)
+            return 0;
+            
+        return RecursiveIteration(root);
+    }
+
+    public int RecursiveIteration(TreeNode node){
+        var height = 1;
+        var leftHeight = 0;
+        var rightHeight = 0;
+
+        if(node.left != null){
+            leftHeight += RecursiveIteration(node.left);
+        }
+        if(node.right != null){
+            rightHeight += RecursiveIteration(node.right);
+        }
+
+        if(leftHeight > rightHeight)
+            height += leftHeight;
+        else
+            height += rightHeight;
+        
+        return height;
+    }
+}
