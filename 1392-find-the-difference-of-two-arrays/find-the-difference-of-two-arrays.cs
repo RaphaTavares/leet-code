c5563e39bf43376d1da2a6bdc00c02e7fb9becad
Raphael Tavares <raphael_tavares2000@hotmail.com>
2024-07-06 13:34:26 -0300

    Time: 151 ms (93.46%) | Memory: 60.7 MB (39.27%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cs
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cs
@@ -1,35 +1,16 @@
+using System.Collections.Generic;
+
 public class Solution {
     public IList<IList<int>> FindDifference(int[] nums1, int[] nums2) {
         var set1 = new HashSet<int>(nums1);
         var set2 = new HashSet<int>(nums2);
 
-        var notIn1 = new List<int>();
-        var notIn2 = new List<int>();
-        
+        var notIn1 = new HashSet<int>(set2);
+        notIn1.ExceptWith(set1);
 
-        foreach(var num in nums2){
-            var setSize = set1.Count();
+        var notIn2 = new HashSet<int>(set1);
+        notIn2.ExceptWith(set2);
 
-            set1.Add(num);
-
-            if(setSize != set1.Count())
-                notIn1.Add(num);
-        }
-
-        foreach(var num in nums1){
-            var setSize = set2.Count();
-
-            set2.Add(num);
-
-            if(setSize != set2.Count())
-                notIn2.Add(num);
-        }
-
-        List<IList<int>> result = new();
-
-        result.Add(notIn2);
-        result.Add(notIn1);
-
-        return result;
+        return new List<IList<int>> { new List<int>(notIn2), new List<int>(notIn1) };
     }
 }
