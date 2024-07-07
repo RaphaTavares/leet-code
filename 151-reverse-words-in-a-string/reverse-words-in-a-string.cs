fc26640d293522274eec5a88e69e53c0d4187e95
Raphael Tavares <raphael_tavares2000@hotmail.com>
2024-07-06 22:23:07 -0300

    Time: 69 ms (11.61%) | Memory: 40.9 MB (62.66%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cs
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cs
@@ -1,11 +1,40 @@
-public class Solution {
-    public string ReverseWords(string s) {
-        s = s.Trim();
+public class Solution
+{
+    public string ReverseWords(string s)
+    {
+        int start = s.Length - 1;
+        int end = s.Length - 1;
+        StringBuilder result = new StringBuilder();
 
-        string[] words = s.Split(new char[] { ' ' },
-        StringSplitOptions.RemoveEmptyEntries);
-        Array.Reverse(words);
+        while (start >= 0)
+        {
+            // Skip spaces
+            while (start >= 0 && s[start] == ' ')
+            {
+                start--;
+                end--;
+            }
 
-        return String.Join(" ", words);
+            // Find the start of the word
+            while (start >= 0 && s[start] != ' ')
+            {
+                start--;
+            }
+
+            // Append the word
+            if (end >= 0)
+            {
+                if (result.Length > 0)
+                {
+                    result.Append(' ');
+                }
+                result.Append(s.Substring(start + 1, end - start));
+            }
+
+            // Move the end to the start of the next word
+            end = start;
         }
+
+        return result.ToString();
     }
+}
