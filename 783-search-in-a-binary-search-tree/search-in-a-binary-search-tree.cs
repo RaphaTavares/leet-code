a9f965866460326339bae2eac48d4862a1b7f9e2
Raphael Tavares <raphael_tavares2000@hotmail.com>
2024-08-04 21:45:28 -0300

    Time: 111 ms (28.88%) | Memory: 50.2 MB (83.50%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cs
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cs
@@ -13,18 +13,15 @@
  */
 public class Solution {
     public TreeNode SearchBST(TreeNode root, int val) {
-        if(root == null)
-            return null;
-
-        if(root.val == val)
-            return root;
-
-        else if(root.val > val)
-            return SearchBST(root.left, val);
-        
-        else if(root.val < val) 
-            return SearchBST(root.right, val);
-            
+        while (root != null) {
+            if (root.val == val) {
+                return root;
+            } else if (root.val > val) {
+                root = root.left;
+            } else {
+                root = root.right;
+            }
+        }
         return null;
     }
 }
