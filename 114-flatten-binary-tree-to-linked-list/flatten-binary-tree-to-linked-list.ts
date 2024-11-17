1d067fb7ca6d1bb827130c1a538600a7f95923db
Raphael Tavares <raphael_tavares2000@hotmail.com>
2024-11-17 00:40:33 -0300

    Time: 34 ms (5.22%) | Memory: 57 MB (5.21%) - LeetSync

---
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.ts
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+/**
+ Do not return anything, modify root in-place instead.
+ */
+function flatten(root: TreeNode | null): void {
+    let prev: TreeNode | null = null;
+
+    const traverse = (node: TreeNode | null): void => {
+        if(!node) return;
+
+        traverse(node.right);
+        traverse(node.left);
+
+    console.log(`I'm in the: ${node.val}`)
+        node.right = prev;
+        node.left = null;
+        prev = node;
+    };
+
+    traverse(root);
+};
