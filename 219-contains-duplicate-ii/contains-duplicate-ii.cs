c405311a183c2f15c2af5e8e1670e0c2f9935560
Raphael Tavares <raphael_tavares2000@hotmail.com>
2024-11-23 14:26:56 -0300

    Time: 11 ms (81.63%) | Memory: 69.2 MB (44.34%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cs
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cs
@@ -1,16 +1,20 @@
 public class Solution {
     public bool ContainsNearbyDuplicate(int[] nums, int k) {
         
+        HashSet<int> window = new();
+
+        int start = 0;
+
         for(int i = 0; i < nums.Length; i++){
-            int counter = 1;
-
-            while(counter <= k)
-            {
-                if(i+counter < nums.Length && nums[i] == nums[i+counter])
-                    return true;
-
-                counter++;
+            if(i - start > k){
+                window.Remove(nums[start]);
+                start++;
             }
+
+            if(window.Contains(nums[i]))
+                return true;
+
+            window.Add(nums[i]);
         }
 
         return false;
