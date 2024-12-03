2925083c0a4dce720400862401792e7f69531193
Raphael Tavares <raphael_tavares2000@hotmail.com>
2024-12-03 16:13:55 -0300

    Time: 11 ms (98.83%) | Memory: 72.4 MB (99.44%) - LeetSync

--- 948-sort-an-array/sort-an-array.cs
+++ 948-sort-an-array/sort-an-array.cs
@@ -1,58 +1,30 @@
 public class Solution {
     public int[] SortArray(int[] nums) {
-        QuickSort(nums, 0, nums.Length - 1);
-        
-        return nums;
-    }
+     var negative = new int[50001];
+     var positive = new int[50001];
+     var result = new int[nums.Length];
 
-    public int[] QuickSort(int[] nums, int start, int end){
-        if(start >= end)
-            return nums;
-
-        int pivotIndex = MedianOfThree(nums, start, end);
-        Swap(nums, pivotIndex, end);
-
-        var pivot = nums[end];
-        var leftPointer = start;
-
-        for(int i = start; i <= end; i++){
-            if(nums[i] < pivot){
-                var temp = nums[leftPointer];
-                nums[leftPointer] = nums[i];
-                nums[i] = temp;
-                leftPointer++;
-            }
-        }
-
-        nums[end] = nums[leftPointer];
-        nums[leftPointer] = pivot;
-
-        QuickSort(nums, start, leftPointer - 1);
-        QuickSort(nums, leftPointer + 1, end);
-
-        return nums; 
-    }
-
-    private void Swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-
-    private int MedianOfThree(int[] nums, int start, int end) {
-        int mid = start + (end - start) / 2;
-
-        // Valores do primeiro, do meio e do último elementos
-        int a = nums[start];
-        int b = nums[mid];
-        int c = nums[end];
-
-        // Determina a mediana entre a, b e c
-        if ((a > b) != (a > c))
-            return start; // 'a' é a mediana
-        else if ((b > a) != (b > c))
-            return mid; // 'b' é a mediana
+     for(int i = 0; i < nums.Length; i++){
+        if(nums[i] >= 0)
+            positive[nums[i]]++;
         else
-            return end; // 'c' é a mediana
+            negative[Math.Abs(nums[i])]++;
+     }
+
+     int index = 0;
+
+     for(int i = negative.Length - 1; i > 0; i--){
+        for(int j = 0; j < negative[i]; j++){
+            result[index++] = -i;
+        }
+     }
+
+     for(int i = 0; i < positive.Length; i++){
+        for(int j = 0; j < positive[i]; j++){
+            result[index++] = i;
+        }
+     }
+
+     return result;
     }
 }
